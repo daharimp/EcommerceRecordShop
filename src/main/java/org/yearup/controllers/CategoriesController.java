02f4f056864c1fa146d8c8d27ef611e03958a458
daharimp <daharimorgan@gmail.com>
2026-06-19 14:34:01 -0700

    Initial commit

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,9 +1,10 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -11,63 +12,67 @@ import org.yearup.service.ProductService;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryService categoryService;
     private ProductService productService;
 
+    @Autowired
+    public CategoriesController(CategoryService categoryService, ProductService productService)
+    {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
 
-    // create an Autowired constructor to inject the categoryService and productService
-
-    // add the appropriate annotation for a get action
+    // GET http://localhost:8080/categories
+    @GetMapping
     public List<Category> getAll()
     {
-        // find and return all categories
-        return null;
+        return categoryService.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
+    // GET http://localhost:8080/categories/1
+    @GetMapping("{id}")
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+        return categoryService.getById(id);
     }
 
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
+    // GET http://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
-        // get a list of product by categoryId
-        return null;
+        // NOTE: verify this method name against your ProductService.
+        // Common names in this starter are getProductsByCategoryId(...) or search(...).
+        return productService.(categoryId);
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
+    // POST http://localhost:8080/categories  (ADMIN only)
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Category> addCategory(@RequestBody Category category)
     {
-        // insert the category and return it with status 201 Created
-        return null;
+        Category created = categoryService.create(category);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    // PUT http://localhost:8080/categories/1  (ADMIN only)
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public Category updateCategory(@PathVariable int id, @RequestBody Category category)
     {
-        // update the category by id and return the updated category (200 OK)
-        return null;
+        return categoryService.update(id, category);
     }
 
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    // DELETE http://localhost:8080/categories/1  (ADMIN only)
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteCategory(@PathVariable int id)
     {
-        // delete the category by id and return status 204 No Content
-        return null;
+        categoryService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
